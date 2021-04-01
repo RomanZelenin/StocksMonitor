3ee598d776fac3c9b4bfc482acde764c08d8256c
Roman Zelenin <roman.zelenin.msk@gmail.com>
2021-04-01 23:41:46 +0300

    Fixing bug with infinty load

--- app/src/main/java/com/romanzelenin/stocksmonitor/StocksRemoteMediator.kt
+++ app/src/main/java/com/romanzelenin/stocksmonitor/StocksRemoteMediator.kt
@@ -1,5 +1,6 @@
 package com.romanzelenin.stocksmonitor
 
+import android.util.Log
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
@@ -20,45 +21,34 @@ class StocksRemoteMediator(private val service: FinService, val db: Repository) 
     ): MediatorResult {
 
         val page = when (loadType) {
-            LoadType.REFRESH -> {
-                state.anchorPosition?.run {
-                    state.closestItemToPosition(this)?.symbol?.let {
-                        db.getRemoteKey(it)?.nextKey?.minus(1)
-                    }
-                } ?: 1
-            }
-            LoadType.PREPEND -> {
-                state.firstItemOrNull()?.symbol?.let {
-                    db.getRemoteKey(it)?.prevKey
-                }
-                    ?: return MediatorResult.Success(endOfPaginationReached = false)
-            }
+            LoadType.REFRESH -> { 1 }
+            LoadType.PREPEND -> { return MediatorResult.Success(endOfPaginationReached = true) }
             LoadType.APPEND -> {
                 state.lastItemOrNull()?.symbol?.let {
                     db.getRemoteKey(it)?.nextKey
                 }
-                    ?: return MediatorResult.Success(endOfPaginationReached = false)
+                    ?: return MediatorResult.Success(endOfPaginationReached = true)
             }
         }
         try {
             val stocks = service.mostActiveStocks(page)
-
             if (stocks != null) {
                 if (loadType == LoadType.REFRESH) {
                     db.clearListTrendingStocks()
                 }
-
-                val prevKey = if (page == 1) null else page - state.config.pageSize
-                val nextKey = if (stocks.isEmpty()) null else page + state.config.pageSize
-                val keys = stocks.map {
-                    RemoteKey(it.symbol, prevKey, nextKey)
-                }
+                val prevKey = if (page == 1) null else page - stocks.size
+                val nextKey = if (stocks.isEmpty()) null else page + stocks.size
+                val keys = stocks.map { RemoteKey(it.symbol, prevKey, nextKey) }
                 db.addTrendingStocksWithRemoteKeys(stocks, keys)
-                return MediatorResult.Success(endOfPaginationReached = stocks.isEmpty())
             }
-            return MediatorResult.Error(IllegalArgumentException())
+            return MediatorResult.Success(endOfPaginationReached = stocks?.isEmpty() ?: true)
         } catch (e: IOException) {
+            Log.d(TAG,e.toString())
             return MediatorResult.Error(e)
         }
+    }
+
+    companion object{
+        private val TAG = StocksRemoteMediator::class.java.simpleName
     }
 }
